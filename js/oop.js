9d830359e4c04277a4d49b0dc41ae5e65749ee4c
Priyansh Thakar <priyanshthakar@gmail.com>
2025-03-05 19:22:34 -0500

    oop complete

--- js/oop.js
+++ js/oop.js
@@ -2,14 +2,42 @@ const output = document.getElementById('output');
 const output2 = document.getElementById('output2');
 
 /* STEP 1a: Create a new object using a regular function */
+function createNewPerson(firstName, lastName, age, gender, interests) {
+    var obj = {};
+    obj.name = {
+        "firstName": firstName,
+        "lastName": lastName
+    }
+    obj.age = age;
+    obj.gender = gender;
+    obj.interests = interests;
+    obj.bio = function () {
+        return `The interests of ${this.name.firstName}, of age ${this.age}, of gender ${this.gender} are ${this.interests}.`;
+    }
+    return obj;
+}
 
 /* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+let person1 = createNewPerson("Priyansh", "Thakar", 16, "Male", ["swimming", "dancing", "cooking"]);
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
 rewrite the above function, without returning anything. Capitalize the name of the function. */
+function Person(firstName, lastName, age, gender, interests) {
+    this.name = {
+        "firstName": firstName,
+        "lastName": lastName
+    }
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function () {
+        return `The interests of ${this.name.firstName}, of age ${this.age}, of gender ${this.gender} are ${this.interests}.`;
+    }
+}
 
 /* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
 and again invoking the .greeting() method for each person */
+let person2 = new Person("Priyansh", "Thakar", 16, "Male", ["swimming", "dancing", "cooking"]);
 
 /* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
  Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
@@ -22,13 +50,17 @@ and again invoking the .greeting() method for each person */
 // person1.bio()
 
 /* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
+let car = new Object();
 
 /* STEP 4b: Once 'car' is created, add various properties and methods… */
+car.brand = "Honda";
+car.model = "Civic";
 
 /* STEP 4c: Change some of the properties of 'car' in the console, then invoke the car.fun() function */
 
 /* STEP 5a: Yet another approach is to use the create() method. 
 Let's see how the above car object can be used to create another object */
+let anotherCar = Object.create(car);
 
 /* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
 
